e0902b6073abc6efe91d56ff2565badf94f6bf43
shanfucha1024 <18701018129@163.com>
2024-04-14 11:27:07 +0800

    message 13: refactor sleep()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,11 +12,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
+    def wait_for_row_inlist_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID,'id_list_table')
+                rows = table.find_elements(By.TAG_NAME,'tr')
+                self.assertIn(row_text,[row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)    
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         #张三听说有一个在线待办事项应用
@@ -40,28 +47,15 @@ class NewVisitorTest(LiveServerTestCase):
         #按回车键后，页面更新了
         #页面中显示了“1: Buy flowers”作为待办事项
         inputbox.send_keys(Keys.ENTER) #(3)
-        time.sleep(1) #(4)
-        self.check_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_inlist_table('1: Buy flowers')
 
-        table = self.browser.find_element(By.ID, 'id_list_table')   
-        rows = table.find_elements(By.TAG_NAME, 'tr')   #(1)
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-        
-
-        #页面中还有一个文本输入框，可以输入其他待办事项
-        #他输入了“Give a gift to Lisi”
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         #页面再次更新，显示了两个待办事项
-        # table = self.browser.find_element(By.ID, 'id_list_table')
-        # rows = table.find_elements(By.TAG_NAME, 'tr')
-        # self.assertIn('1: Buy flowers', [row.text for row in rows])
-        # self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
-        self.check_for_row_in_list_table('1: Buy flowers')
-        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+        self.wait_for_row_inlist_table('1: Buy flowers')
+        self.wait_for_row_inlist_table('2: Give a gift to Lisi')
 
         #他想知道这个网站是否会记住他的待办事项
         #他看到网站为他生成了一个唯一的URL
