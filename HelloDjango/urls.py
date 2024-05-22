f1f9ba21f2f6d6ff79dfe6b1ec5ebcd3808104cd
shanfucha1024 <18701018129@163.com>
2024-05-22 15:28:05 +0800

    message 18:new URL and view for adding to existing lists.UT and FT pass

--- HelloDjango/urls.py
+++ HelloDjango/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('lists/new', views.new_list, name='new_list'),
     re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
     re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item')
-
 ]
