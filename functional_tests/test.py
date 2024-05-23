e31977ace712b32b5e1a43a8c77bea3144e8cc68
shanfucha1024 <18701018129@163.com>
2024-05-23 13:08:01 +0800

    message 22: use Bootstrap to improve layout

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 from selenium.webdriver.common.by import By
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+import os
 
 MAX_WAIT = 10
+class NewVisitorTest(StaticLiveServerTestCase):
 
-class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
-    
+        real_server = os.environ.get('REAL_SERVER')
+        if real_server:
+            self.live_server_url = 'http://' + real_server
+
     def tearDown(self):
         self.browser.quit()
 
@@ -21,7 +24,7 @@ class NewVisitorTest(LiveServerTestCase):
             try:
                 table = self.browser.find_element(By.ID, 'id_list_table')
                 rows = table.find_elements(By.TAG_NAME, 'tr')
-                self.assertIn(row_text, [row.text for row in rows])
+                self.assertIn(row_text, [row_text for row in rows])
                 return
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
@@ -29,86 +32,86 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-
-        #张三听说有一个在线待办事项应用
-        #他去看了这个应用的首页
         self.browser.get(self.live_server_url)
 
-        #网页包含'To-Do'这个词
-        self.assertIn('To-Do', self.browser.title), "Browser title was " + self.browser.title
-        header_text = self.browser.find_element(By.TAG_NAME,'h1').text #(1)
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
-        #应用有一个输入待办事项的文本输入框
+        # 应用有一个输入待办事项的文本输入框
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-        #在文本输入框中输入了“Buy flowers”
-        inputbox.send_keys('Buy flowers') #(2)
 
-        #按回车键后，页面更新了
-        #页面中显示了“1: Buy flowers”作为待办事项
-        inputbox.send_keys(Keys.ENTER) #(3)
+        # 他在文本输入框中输入了 ”Buy Flowers"
+        inputbox.send_keys('Buy flowers')
+
+        # 他按了回车键后，页面更新了
+        # 待办事项表格中显示了“1: Buy Flowers"
+        inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy flowers')
-        
 
-        #页面中还有一个文本输入框，可以输入其他待办事项
-        #他输入了“Send a gift to Lisi”
+        # 页面中又显示了一个文本输入框，可以输入其他待办事项
+        # 他又输入了”Send A Gift to Lisa"
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        inputbox.send_keys('Send a gift to Lisi')
+        inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
 
-        #页面再次更新，显示了两个待办事项
+        # 页面再次刷新，他的清单中显示了这两个待办事项
         self.wait_for_row_in_list_table('1: Buy flowers')
-        self.wait_for_row_in_list_table('2: Send a gift to Lisi')
+        self.wait_for_row_in_list_table('2: Give a gift to Lisi')
 
-        #他很满意，去睡觉了
+        # 张三想知道这个网站是否会记住他的清单
+        # 他看到网站为他生成了一个唯一的URL
+        # self.fail('Finish the test!')
+
+        # 他访问哪个URL，发现它的待办事项列表还在
+        # 他满意地离开了
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        #张三新建一个待办事项清单
+        # 张三新建一个待办事项清单
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Buy flowers')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy flowers')
 
-        #他注意到清单有一个唯一的URL
+        # 他注意到清单有个唯一的URL
         zhangsan_list_url = self.browser.current_url
         self.assertRegex(zhangsan_list_url, '/lists/.+')
 
-        #现在有一个叫做王五的新用户访问了网站
-        ##我们使用一个新的浏览器会话
-        ##确保张三的信息不会从cookie中泄露出来
+        # 现在一个新用户王五访问网站
+        # 我们使用——个新浏览器会话
+        # 确保张三的信息不会从cookie中世露出去
         self.browser.quit()
         self.browser = webdriver.Chrome()
 
-        #王五访问首页
-        #首页中没有张三的待办事项
+        # 王五访问首页
+        # 页面中看不到张三的清单
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Buy flowers', page_text)
-        self.assertNotIn('Send a gift to Lisi', page_text)
+        self.assertNotIn('Give a gift to Lisi', page_text)
 
-        #王五输入一个新的待办事项，新建一个清单
+        # 王五输入一个新代办事项， 新建一个清单
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
-        #王五获得了他的唯一URL
+        # 王五获得了他的唯一URL
         wangwu_list_url = self.browser.current_url
         self.assertRegex(wangwu_list_url, '/lists/.+')
         self.assertNotEqual(wangwu_list_url, zhangsan_list_url)
 
-        #这个页面中还是没有张三的待办事项
+        # 这个页面还是没有张三的清单
         page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Buy flowers', page_text)
         self.assertIn('Buy milk', page_text)
 
-        #两人都很满意，去睡觉了
-        
+        # 两人都很满意，然后去睡觉了
     def test_layout_and_styling(self):
         # 张三访问首页
         self.browser.get(self.live_server_url)
@@ -132,3 +135,5 @@ class NewVisitorTest(LiveServerTestCase):
             512,
             delta=10
         )
+
+
